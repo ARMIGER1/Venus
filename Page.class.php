c15fb2cef4bdc00e2e83ac5df794ce7a33919ac0
Nick Smith <1hockeyplayer+github@gmail.com>
2011-10-14 23:21:42 -0700

    Added a base class for all other files.
    A test function has been added as well, to start it off.

---
+++ Page.class.php
@@ -0,0 +1,23 @@
+<?php
+	/**
+	 * 
+	 * @brief Handles all global page functions.
+	 * @author Nick Smith <1hockeyplayer+github@gmail.com>
+	 * @copyright Copyright (c) 2011, Nick Smith
+	 * @version 1.0
+	 *
+	 */
+	class Page {
+		
+		/**
+		 * 
+		 * @brief Verifies that the class has been included and is in fact working.
+		 */
+		function test() {
+			
+			echo "<div style=\"border: 1px solid white; width: auto; float: left; color: white; padding: 1%; background(200,200,200,0.5);\">Page.class.php Test Success!</div>";
+			
+		}
+		
+	}
+?>
